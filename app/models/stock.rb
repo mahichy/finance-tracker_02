aeda2a0739615e8eeeee6ddfd2eb260d356a1efc
mahi <mohiddinoshim@yahoo.com>
2020-09-12 15:26:01 +0600

    Stock lookup and secure Api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+	def self.new_lookup(ticker_symbol)
+		client = IEX::Api::Client.new(publishable_token: ENV["publishable_token"],
+  									  secret_token: ENV["secret_token"],
+  									  endpoint: 'https://sandbox.iexapis.com/v1')
+		client.price(ticker_symbol)
+	end
 end
