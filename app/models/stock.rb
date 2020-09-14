4a4cc9e2e96dd07d4ca51df514395b0ee0c08a60
mahi <mohiddinoshim@yahoo.com>
2020-09-14 15:47:54 +0600

    Dealing with invalid search resault

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,11 @@ class Stock < ApplicationRecord
 		client = IEX::Api::Client.new(publishable_token: ENV["publishable_token"],
   									  secret_token: ENV["secret_token"],
   									  endpoint: 'https://sandbox.iexapis.com/v1')
-		new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+		begin
+								  new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+		rescue => exception
+		return nil	
+		
+		end							  
 	end
 end
