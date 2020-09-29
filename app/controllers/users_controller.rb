0c9736dbe2119517de33ed91cea3c43c058f8daa
mahi <mohiddinoshim@yahoo.com>
2020-09-29 16:06:52 +0600

    Display search result in the browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
 
   def search
   			if params[:friend].present?
-			@friend = params[:friend]
+			@friends = User.search(params[:friend])
+			@friends = current_user.except_current_user(@friends)
 			
-				if @friend
+				if @friends
 					respond_to do |format|
 						format.js { render partial: 'users/friend_result'}
 					end
